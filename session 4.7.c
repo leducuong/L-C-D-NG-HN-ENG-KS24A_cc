e5a3ea7fbbac90711636b7ee719e8345014bf76e
leducuong <leduongchienhoc@gmail.com>
2024-11-25 00:17:25 +0700

    Add files via upload

---
+++ session 4.7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("NhAp vAo so nam: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("Nam %d là nam nhuan.\n", year);
+    } else {
+        printf("Nam %d khong phai là nam nhuan.\n", year);
+    }
+
+    return 0;
+}
